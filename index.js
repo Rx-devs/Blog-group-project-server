89fd978b7772af2cfbaebb327f9211d005a96d04
Tonmoy Roy <rxdevs100@gmail.com>
2022-01-07 20:11:35 +0600

    single blog search

--- index.js
+++ index.js
@@ -51,8 +51,8 @@ async function run() {
         // get a single blog
         app.get('/allblogs/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const blog = await allBlogsCollection.findOne(query);
+            // const query = { _id: ObjectId(id) };
+            const blog = await allBlogsCollection.findOne(id);
             res.json(blog);
         });
 		// get a single carrier blog
